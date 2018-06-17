a2c4986aa08ee62f0943d2e47c9a3fa82c063be2
wangzhen <thewhiteelephant@163.com>
2018-06-18 00:12:55 +0800

    简单的实现了，tabular输出select查询到的数据，但是非常的不理想，破坏了很多的函数的参数形式和个数（原型）

--- c/linux4/database/mysql/sql.c
+++ c/linux4/database/mysql/sql.c
@@ -3,17 +3,25 @@
 #include <string.h>
 #include <mysql.h>
 
+typedef struct  {
+	char *name;
+	unsigned long length;
+} field_type, *field_type_pointer;
+
 int insert(MYSQL * , char* , char* , int);
 int update_age(MYSQL*, char*, char*, int);
 void action_error(MYSQL*, char*);
 void retrieve_by_age_by_all(MYSQL*, char*, int);
 void retrieve_by_age_one_by_one(MYSQL*, char*, int);
-void display_row(MYSQL*, MYSQL_ROW);
+void display_row(MYSQL*, MYSQL_ROW, field_type_pointer);
+field_type_pointer display_header(MYSQL_RES* ,int*);
+
 
 int main(){
 	//MYSQL *conn_ptr;
 	MYSQL connection;
 	int res;
+	field_type_pointer base; //
 	if( !mysql_init(&connection) ){
 		fprintf(stderr,"mysql_init failed \n");
 		return EXIT_FAILURE;
@@ -114,7 +122,7 @@ void retrieve_by_age_by_all(MYSQL* connection_ptr, char* table_name, int age ){
 	printf("we got %lu rows\n", (unsigned long)total_rows);
 	while( ( sqlrow = mysql_fetch_row(result) ) != NULL  ){
 		printf("fetched data\n");
-		display_row(connection_ptr, sqlrow);
+		display_row(connection_ptr, sqlrow,NULL); //will be wrong 
 	};
 	if(mysql_errno(connection_ptr)){
 		//error
@@ -128,12 +136,14 @@ void retrieve_by_age_by_all(MYSQL* connection_ptr, char* table_name, int age ){
 
 void retrieve_by_age_one_by_one(MYSQL* connection_ptr, char* table_name, int age ){
 	int res;
+	int result_len;
+	int i = 0;
 	MYSQL_RES *result;
 	MYSQL_ROW sqlrow;
 	my_ulonglong total_rows;
-
+	field_type_pointer fields_simplify;
 	char *temp_ptr = malloc(1024);
-	sprintf(temp_ptr, "SELECT number,name,age,gender FROM %s WHERE age=%d ", table_name, age);	
+	sprintf(temp_ptr, "SELECT * FROM %s WHERE age=%d ", table_name, age);	
 	res = mysql_query(connection_ptr, temp_ptr);
 	if(res){
 		action_error(connection_ptr, "SELECT");
@@ -143,7 +153,7 @@ void retrieve_by_age_one_by_one(MYSQL* connection_ptr, char* table_name, int age
 	free(temp_ptr);
 	result = mysql_use_result(connection_ptr);
 	if(!result){
-		action_error(connection_ptr,"mysql_store_data");
+		action_error(connection_ptr,"mysql_use_data");
 		return;
 	}
 	//because mysql_use_result will retrieve all matched rows one by one with every request to mysql_fetch_row() 
@@ -151,10 +161,16 @@ void retrieve_by_age_one_by_one(MYSQL* connection_ptr, char* table_name, int age
 
 	//total_rows = mysql_num_rows(result);
 	//printf("we got %lu rows\n", (unsigned long)total_rows);
-
+	fields_simplify = display_header(result, &result_len);
+	//printf("\n%s \t%lu \t%d\n",fields_simplify[0].name, fields_simplify[0].length, result_len);
+	while(i < result_len){
+		printf("%*s", fields_simplify[i].length, fields_simplify[i].name);
+		i++;
+	}
+	printf("\n");
+	i=0;	
 	while( ( sqlrow = mysql_fetch_row(result) ) != NULL  ){
-		printf("fetched data\n");	
-		display_row(connection_ptr, sqlrow);
+		display_row(connection_ptr, sqlrow, fields_simplify);
 	};
 	if(mysql_errno(connection_ptr)){
 		//error
@@ -162,18 +178,68 @@ void retrieve_by_age_one_by_one(MYSQL* connection_ptr, char* table_name, int age
 		mysql_free_result(result);
 		return;
 	}
+	free(fields_simplify);
 	mysql_free_result(result);
 	return ;
 } 
 
 
-void display_row(MYSQL* connection_ptr, MYSQL_ROW sqlrow){
+void display_row(MYSQL* connection_ptr, MYSQL_ROW sqlrow, field_type_pointer fp){
 	int field_count = 0;
 	int i = 0;
 	field_count = mysql_field_count(connection_ptr);
 	while( i < field_count){
-		printf("%-15s \t",sqlrow[i]);
+		printf("%*s", fp[i].length, sqlrow[i]);
 		i++;
 	}
 	printf("\n");
+}
+
+field_type_pointer display_header(MYSQL_RES* res_ptr,int *len){
+	MYSQL_FIELD* field_ptr;
+	field_type_pointer fields_base;
+	field_type temp_field;
+	int length = 0;
+	mysql_field_seek(res_ptr, 0); //every time ,the move to the begining of fields;
+	printf("Colunm detail: \n");
+	//field_ptr = mysql_fetch_fields(res_ptr);
+	length = mysql_num_fields(res_ptr);
+	// printf("field number: %d\n", length);
+	
+
+	fields_base = ( field_type_pointer )malloc( length * sizeof( field_type ) );
+	length = 0;
+
+	while( (field_ptr = mysql_fetch_field(res_ptr) ) != NULL){
+		// printf("\tName: %s\n", field_ptr->name);
+		// printf("\tType: ");
+		// if(IS_NUM( field_ptr->type)){
+		// 	printf("Numeric field\n");
+		// }else{
+		// 	switch(field_ptr->type){
+		// 		case FIELD_TYPE_LONG:
+		// 			printf("Long\n");
+		// 			break;
+		// 		case FIELD_TYPE_VAR_STRING:
+		// 			printf("VARCHAR\n");
+		// 			break;
+		// 		default:
+		// 			printf("is %d, check in mysql.com.h\n",field_ptr->type); //type is %d
+		// 	}
+		// }
+
+		// printf("\tWidth %ld\n", field_ptr->length);
+		// printf("\tMax-length %ld\n", field_ptr->max_length);
+		// if(field_ptr->flags & AUTO_INCREMENT_FLAG){
+		// 	printf("\tAuto increments\n");
+		// }
+		printf("%s  %lu\n", field_ptr->name, field_ptr->length);
+		temp_field.name = field_ptr->name;
+		temp_field.length = field_ptr->length;
+		fields_base[length] = temp_field;
+		length++;
+		// printf("\n");
+	}
+	*len = length;
+	return fields_base;
 }
